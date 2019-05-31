91d9716f7b7cd63671e21fc5c04f5c611304b971
Joe <38414120+jazzathoth@users.noreply.github.com>
2019-05-31 15:10:24 -0400

    Update acme_test.py

--- sprint31/acme_test.py
+++ sprint31/acme_test.py
@@ -20,5 +20,23 @@ class AcmeProductTests(unittest.TestCase):
         self.assertEqual(prod.explode(), '...BABOOM!!')
         self.assertEqual(prod.stealability(), 'Very stealable.')
 
+
+    def test_types(self):
+        prod = Product('Test 3')
+        assert(type(prod.price) == int)
+
+    def test_default_num_products(self):
+        prods = generate_products()
+        self.assertEqual(len(prods), 30)
+
+    def test_legal_names(self):
+        prods = generate_products()
+        NOUNS = [['Anvil'], ['Catapult'], ['Disguise'], ['Mousetrap'], ['???']]
+        ADJECTIVES = [['Awesome'], ['Shiny'], ['Impressive'], ['Portable'], ['Improved']]
+        for p in prods:
+            assert(p.name.split()[0] in ADJECTIVES)
+            assert(p.name.split()[1] in NOUNS)
+
+
 if __name__ == '__main__':
     unittest.main()
